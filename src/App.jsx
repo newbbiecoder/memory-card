a72bb0c844c7689a2144cea5d5f3d94b60c0f8a0
Rudraksh <rudrakshkumar2908@gmail.com>
2025-11-09 14:47:09 +0000

    Made an asynchronous getPokemon function which generates a random number and pass it as id to the API. Made 10 such api calls and stored all promises in requests array using Promise.all, then updated state items and dataIsLoaded after all the Promises are fetched Called ShowPokemon component in the main function component which stores name and url of pokemon in pokemon array, then maps over that array to display images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,61 @@
-function App() {
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function getPokemons() {
+      const requests = [];
+
+      for(let i = 0; i < 10; i++) {
+        const randomNumber = Math.floor(Math.random() * 1025) + 1;
+        requests.push(
+          fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then((res) => res.json())
+        );
+      }
+
+      const results = await Promise.all(requests);
+      setItems(results);
+      setDataIsLoaded(true);
+    }
+    getPokemons();
+  },[]);
+  if(!dataIsLoaded) {
+    return (
+      <div>
+        <h2>Please wait for some time...</h2>
+      </div>
+    )
+  }
+
   return (
-    <h1 style={{color: 'white'}}>HELLO WORLD</h1>
+    <>
+      <h1 style={{color: 'white'}}>HELLo</h1>
+      <div>
+        <ShowPokemon items={items} /> 
+      </div>
+    </>
   )
 }
 
-export default App
+function ShowPokemon({items}) {
+  const pokemon = []
+  return (
+    <>  
+      <div className="container">
+        {
+          items.map((item) => {
+            pokemon.push({name: item.species.name, url: item.sprites.front_default})
+          })
+        }
+        {pokemon.map(selectPokemon => {
+          return (
+            <img src={selectPokemon.url} alt={selectPokemon.name} key={selectPokemon.name}/>
+          )
+        })}
+      </div>
+    </>
+    // <img src={items.sprites.front_default} alt={items.species.name} />
+  )
+}
